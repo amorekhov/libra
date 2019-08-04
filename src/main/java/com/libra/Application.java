63be33d8856bfbe92ac5dbf9ac0f93b2abcec77c
amorekhov <alekhandr@mail.ru>
2019-08-04 14:13:39 +0300

    add application.java

---
+++ src/main/java/com/libra/Application.java
@@ -0,0 +1,10 @@
+package com.libra;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
