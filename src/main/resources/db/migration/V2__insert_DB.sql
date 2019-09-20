236ec1df83b8b9f40410991b783259559366530c
amorekhov <alekhandr@mail.ru>
2019-09-20 13:46:00 +0300

    add data book

--- src/main/resources/db/migration/V2__insert_DB.sql
+++ src/main/resources/db/migration/V2__insert_DB.sql
@@ -5,30 +5,30 @@ insert into user_role(user_id, roles)
             values (1, 'USER'), (1, 'ADMIN');
 
 insert into book (book_id, author, name,  description, availability )
-        values (2,'','','', true);
+        values (2,'Джошуа Блох','Эффективная Java','', true);
 
 insert into book (book_id, author, name,  description, availability )
-        values (3,'','','', true);
+        values (3,'Robert C. Martin','Clean Code: A Handbook of Agile Software Craftsmanship','', true);
 
  insert into book (book_id, author, name,  description, availability )
-         values (4,'','','', true);
+         values (4,'Кей С. Хорстманн','Java. Библиотека профессионала','', true);
 insert into book (book_id, author, name,  description, availability )
-        values (5,'','','', true);
+        values (5,'Эрик Фримен, Элизабет Робсон','Head First. Паттерны проектирования','', true);
 insert into book (book_id, author, name,  description, availability )
-        values (6,'','','', true);
+        values (6,'Крейг Уоллс','Spring в действии','', true);
 
 insert into book (book_id, author, name,  description, availability )
-        values (7,'','','', true);
+        values (7,'Lasse Koskela','Test Driven: TDD and Acceptance TDD for Java Developers','', true);
 
 insert into book (book_id, author, name,  description, availability )
-               values (8,'','','', true);
+               values (8,'Герберт Шилдт','Java. Полное руководство','', true);
 
 
 insert into book (book_id, author, name,  description, availability )
-        values (9,'','','', true);
+        values (9,'Кэти Сиерра, Берт Бейтс','Изучаем Java','', true);
 
 insert into book (book_id, author, name,  description, availability )
-        values (10,'','','', true);
+        values (10,'Роберт Седжвик, Кевин Уэйн','Алгоритмы на Java','', true);
 
 insert into book (book_id, author, name,  description, availability )
-        values (11,'','','', true);
+        values (11,'Brett D. McLaughlin','Head First Object-Oriented Analysis and Design','', true);
